621c2e049b6b65e79dccda3e57ab82e64c09f437
Grzegorz <69852389+Nebedr@users.noreply.github.com>
2021-03-22 14:03:45 +0100

    Add files via upload

---
+++ bars.js
@@ -0,0 +1,108 @@
+/* Examples */
+(function($) {
+  /*
+   * Example 1:
+   *
+   * - no animation
+   * - custom gradient
+   *
+   * By the way - you may specify more than 2 colors for the gradient
+   */
+  $('.first.circle').circleProgress({
+    value: 0.85,
+    fill: {gradient: [['#ff5733', .5], ['#ff5733', .5]], gradientAngle: Math.PI / 4}
+  }).on('circle-animation-progress', function(event, progress, stepValue) {
+    $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+  });
+
+  /*
+   * Example 2:
+   *
+   * - default gradient
+   * - listening to `circle-animation-progress` event and display the animation progress: from 0 to 100%
+   */
+  $('.second.circle').circleProgress({
+    value: 0.85,
+    fill: {gradient: [['#0681c4', .5], ['#0681c4', .5]], gradientAngle: Math.PI / 4}
+  }).on('circle-animation-progress', function(event, progress, stepValue) {
+    $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+  });
+
+  /*
+   * Example 3:
+   *
+   * - very custom gradient
+   * - listening to `circle-animation-progress` event and display the dynamic change of the value: from 0 to 0.8
+   */
+ 
+
+
+  /*
+   * Example 4:
+   *
+   * - solid color fill
+   * - custom start angle
+   * - custom line cap
+   * - dynamic value set
+   */
+ $
+
+  /*
+   * Example 5:
+   *
+   * - image fill; image should be squared; it will be stretched to SxS size, where S - size of the widget
+   * - fallback color fill (when image is not loaded)
+   * - custom widget size (default is 100px)
+   * - custom circle thickness (default is 1/14 of the size)
+   * - reverse drawing mode
+   * - custom animation start value
+   * - usage of "data-" attributes
+   */
+  $('.third.circle').circleProgress({
+    value: 0.7,
+    fill: {gradient: [['white', .5], ['#6a4e86', .5]], gradientAngle: Math.PI / 4}
+  }).on('circle-animation-progress', function(event, progress, stepValue) {
+    $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+  });
+
+  // Let's emulate dynamic value update
+  
+  setTimeout(function() { c4.circleProgress('value', 1.0); }, 1100);
+  setTimeout(function() { c4.circleProgress('value', 0.5); }, 2100);
+  
+  $('.forth.circle').circleProgress({
+    value: 0.5,
+    fill: {gradient: [['white', .5], ['#d3cc3a', .5]], gradientAngle: Math.PI / 4}
+  }).on('circle-animation-progress', function(event, progress, stepValue) {
+    $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+  });
+  
+  $('.fifth.circle').circleProgress({
+    value: 0.55,
+    fill: {gradient: [['#5d9fe1', .5], ['#0681c4', .5]], gradientAngle: Math.PI / 4}
+  }).on('circle-animation-progress', function(event, progress, stepValue) {
+    $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+  });
+  
+  $('.sixth.circle').circleProgress({
+    value: 0.25,
+    fill: {gradient: [['#0681c4', .5], ['#0681c4', .5]], gradientAngle: Math.PI / 4}
+  }).on('circle-animation-progress', function(event, progress, stepValue) {
+    $(this).find('strong').text(stepValue.toFixed(2).substr(1));
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+})(jQuery);
+
+
